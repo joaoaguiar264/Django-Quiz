524a3606eff8fb1ad24f79e8338733288f93ee26
JOAO VINICIUS DE AGUIAR <joao.aguiar@alunos.sc.senac.br>
2023-12-05 21:22:38 -0300

    quase tudo

--- django_quiz/urls.py
+++ django_quiz/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('', views.index, name='home'),
     path('search-survey/', views.search_survey, name='search_survey'),
     path('add-survey/', views.add_survey, name='add_survey'),
+    path('my-surveys/', views.my_surveys, name='my_surveys'),
+    path('survey-details/<int:id>', views.survey_details, name='survey_details'),
+    path('answer-survey/<int:id>', views.answer_survey, name='answer_survey')
 ]
--- django_quiz/views.py
+++ django_quiz/views.py
@@ -17,7 +17,7 @@ def add_survey(request):
     if request.method == 'POST':
         title = request.POST.get('title')
         question = request.POST.get('question')
-        option1 = request.FILES.get('option1')
+        option1 = request.POST.get('option1')
         option2 = request.POST.get('option2')
         option3 = request.POST.get('option3')
         option4 = request.POST.get('option4')
@@ -36,3 +36,15 @@ def add_survey(request):
         return redirect('home')
     else:
         return render(request, 'pages/add-survey.html')
+
+def survey_details(request, id):
+    survey = Survey.objects.get(id=id)
+    return render(request, 'pages/survey-details.html', {'survey':survey})
+
+def my_surveys(request):
+    surveys = Survey.objects.filter(user_id=request.user.id)
+    return render(request, 'pages/my-surveys.html', {'surveys':surveys})
+
+def answer_survey(request, id):
+    survey = Survey.objects.get(id=id)
+    return render(request, 'pages/survey-details.html', {'survey':survey})
